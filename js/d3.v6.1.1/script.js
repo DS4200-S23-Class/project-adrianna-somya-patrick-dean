35356d915d8e22382594ea0481b72f0f2a62f3a0
Dean Frame <dacframe@gmail.com>
2023-03-22 03:19:31 -0400

    pm03 done

--- js/d3.v6.1.1/script.js
+++ js/d3.v6.1.1/script.js
@@ -1,43 +1,12 @@
 // storing users' axes selections
 function getXAxis() {
-  let xAxis = document.getElementById("axisX").value;
-  console.log(xAxis);
+  const xAxis = document.getElementById("axisX").value;
   return xAxis;
 }
 
 function getYAxis() {
-  let yAxis = document.getElementById("axisY").value;
-  // console.log(yAxis);
+  const yAxis = document.getElementById("axisY").value;
   return yAxis;
-}
-
-// changing the axes will update the graph
-function updateGraph() {
-  svg.selectAll("circle").remove();
-
-  // reading in data
-  return d3.csv("track_data.csv").then((data) => {
-    // plot
-    svg
-      .append("g")
-      .selectAll("dot")
-      .filter((d) => {
-        let num = Math.random();
-        return num < 0.05;
-      })
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("cx", function (d) {
-        return plot(d, xScale, getXAxis);
-      })
-      .attr("cy", function (d) {
-        return plot(d, yScale, getYAxis);
-      })
-      .attr("r", 2)
-      .attr("transform", "translate(" + 100 + "," + 100 + ")")
-      .style("fill", "palevioletred");
-  });
 }
 
 // plot the given data row onto the graph given a scale function and axis calculating function
@@ -69,7 +38,7 @@ function plot(d, scaleFunction, axisFunction) {
 }
 
 // frame for scatterplot
-let svg = d3
+const svg = d3
     .select("#vis1")
     .append("svg")
     .attr("width", 1000)
@@ -79,11 +48,13 @@ let svg = d3
   height = svg.attr("height") - margin;
 
 // scaling function
-let xScale = d3.scaleLinear().domain([0, 100]).range([0, width]),
+const xScale = d3.scaleLinear().domain([0, 100]).range([0, width]),
   yScale = d3.scaleLinear().domain([0, 100]).range([height, 0]);
 
 // axes for scatterplot
-let g = svg.append("g").attr("transform", "translate(" + 100 + "," + 100 + ")");
+const g = svg
+  .append("g")
+  .attr("transform", "translate(" + 100 + "," + 100 + ")");
 
 g.append("g")
   .attr("transform", "translate(0," + height + ")")
@@ -91,7 +62,84 @@ g.append("g")
 
 g.append("g").call(d3.axisLeft(yScale));
 
-// reading in the data
+const TOOLTIP = d3
+  .select("#vis1")
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+function handleMouseover(event, d) {
+  event.target.style.fill = "#1DB954";
+  TOOLTIP.style("opacity", 1);
+}
+
+function handleMouseleave(event, d) {
+  event.target.style.fill = "palevioletred";
+  TOOLTIP.style("opacity", 0);
+}
+
+function handleMousemove(event, d) {
+  let xValue = d[getXAxis()];
+  let yValue = d[getYAxis()];
+  if (getXAxis() !== "popularity" && getXAxis() !== "tempo") {
+    xValue = (xValue * 100).toFixed(2);
+  }
+  if (getYAxis() !== "popularity" && getYAxis() !== "tempo") {
+    yValue = (yValue * 100).toFixed(2);
+  }
+
+  TOOLTIP.html(
+    "track name: " +
+      d.track_name +
+      "<br/>artist name: " +
+      d.artist_name +
+      `<br/>${getXAxis()}: ${xValue}` +
+      `<br/>${getYAxis()}: ${yValue}`
+  )
+    .style("left", event.pageX + 10 + "px")
+    .style("top", event.pageY - 50 + "px");
+}
+
+// read csv and draw scatter plot points
+function drawScatterplotPoints() {
+  // remove any previous points
+  svg.selectAll("circle").remove();
+
+  // reading in data
+  d3.csv("track_data.csv").then((data) => {
+    // plot
+    svg
+      .append("g")
+      .selectAll("dot")
+      .data(
+        data.filter((_) => {
+          const num = Math.random();
+          return num < 0.05;
+        })
+      )
+      .enter()
+      .append("circle")
+      .attr("cx", function (d) {
+        return plot(d, xScale, getXAxis);
+      })
+      .attr("cy", function (d) {
+        return plot(d, yScale, getYAxis);
+      })
+      .attr("r", 3)
+      .attr("transform", "translate(" + 100 + "," + 100 + ")")
+      .style("fill", "palevioletred")
+      .on("mouseover", handleMouseover)
+      .on("mouseleave", handleMouseleave)
+      .on("mousemove", handleMousemove);
+  });
+}
+
+// changing the axes will update the graph
+function updateScatterplot() {
+  drawScatterplotPoints();
+}
+
+// initial reading of the data
 d3.csv("track_data.csv").then((data) => {
   // logging 10 lines of data
   for (i = 0; i < 10; i++) {
@@ -99,23 +147,136 @@ d3.csv("track_data.csv").then((data) => {
   }
 
   // plot
-  svg
-    .append("g")
-    .selectAll("dot")
-    .filter((d) => {
-      let num = Math.random();
-      return num < 0.05;
-    })
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", function (d) {
-      return plot(d, xScale, getXAxis);
-    })
-    .attr("cy", function (d) {
-      return plot(d, yScale, getYAxis);
-    })
-    .attr("r", 2)
-    .attr("transform", "translate(" + 100 + "," + 100 + ")")
-    .style("fill", "palevioletred");
+  drawScatterplotPoints();
 });
+
+// get playlist user has selected
+function getPlaylist() {
+  const playlist = document.getElementById("playlistSelect").value;
+  return playlist;
+}
+// bar chart
+const barChart = d3
+    .select("#vis2")
+    .append("svg")
+    .attr("height", 400)
+    .attr("width", 900),
+  barChartMargin = 40,
+  barChartWidth = barChart.attr("width") - barChartMargin,
+  barChartHeight = barChart.attr("height") - barChartMargin;
+
+// list of attributes to display in bar chart
+const attributes = [
+  "popularity",
+  "danceability",
+  "energy",
+  "speechiness",
+  "instrumentalness",
+  "loudness",
+  "acousticness",
+  "liveness",
+  "valence",
+];
+
+const scaleBarXAxis = d3
+  .scaleBand()
+  .domain(attributes)
+  .range([0, barChartWidth - barChartMargin]);
+const scaleBarYAxis = d3
+  .scaleLinear()
+  .domain([0, 100])
+  .range([barChartHeight - barChartMargin, 0]);
+// x axis
+const barChartAxes = barChart
+  .append("g")
+  .attr("transform", `translate(${barChartMargin},${barChartHeight})`)
+  .call(d3.axisBottom(scaleBarXAxis));
+// y axis
+barChartAxes
+  .append("g")
+  .attr("transform", `translate(0, ${-(barChartHeight - barChartMargin)})`)
+  .call(d3.axisLeft(scaleBarYAxis));
+
+// draw the bars on the bar chart
+// reads from csv for now
+function drawBarChartBars() {
+  d3.csv("track_data.csv").then((data) => {
+    // remove previous bars
+    barChart.selectAll("rect").remove();
+
+    // choose songs based on selected playlist
+    // will be changed
+    let songs = [];
+    switch (getPlaylist()) {
+      case "likedsongs":
+        data.forEach((d, i) => {
+          if (i % 150 === 0) {
+            songs.push(d);
+          }
+        });
+        break;
+      case "chillvibes":
+        songs = data.filter((d) => d.energy < 0.5);
+        break;
+      case "tswift":
+        songs = data.filter((d) => d.artist_name.includes("Taylor Swift"));
+        break;
+      case "garbage":
+        songs = data.filter((d) => d.artist_name.includes("AJR"));
+    }
+
+    // construct each bar height in the graph by calculating averages
+    // function to calculate average
+    function calcAverage(songs, attribute) {
+      let avg = 0;
+      songs.forEach((song) => {
+        let songAttribute = Number(song[attribute]);
+        if (attribute !== "popularity") {
+          songAttribute = songAttribute * 100;
+        }
+        avg += songAttribute;
+      });
+      avg /= songs.length;
+      return avg;
+    }
+    // bars is just a list of numbers representing averages in each attribute
+    const bars = [];
+    attributes.forEach((attribute) => {
+      bars.push(calcAverage(songs, attribute));
+    });
+
+    // scale function for height of each bar
+    const barHeightScale = d3
+      .scaleLinear()
+      .domain([0, 100])
+      .range([0, barChartHeight - barChartMargin]);
+      
+    // draw bars
+    barChart
+      .append("g")
+      .selectAll("rect")
+      .data(bars)
+      .enter()
+      .append("rect")
+      .attr("width", scaleBarXAxis.bandwidth())
+      .attr("height", function (d) {
+        return barHeightScale(d);
+      })
+      .attr("x", function (_, i) {
+        return barChartMargin + scaleBarXAxis.bandwidth() * i;
+      })
+      .attr("y", function (d) {
+        return barChartHeight - barHeightScale(d);
+      })
+      .attr("fill", "#1DB954") // spotify green :)
+      .attr("stroke", "black");
+  });
+}
+
+// changing user playlist will update the graph
+function updateBarChart() {
+  drawBarChartBars();
+}
+
+// initial read of data
+drawBarChartBars();
